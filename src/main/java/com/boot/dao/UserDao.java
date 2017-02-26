4d77e92f3cdc64afaa4350f6e8420587361e45e4
L <1451360190@qq.com>
2017-02-26 22:09:00 +0800

    Spring Boot + JPA + Templates

--- src/main/java/com/boot/dao/UserDao.java
+++ src/main/java/com/boot/dao/UserDao.java
@@ -1,12 +1,10 @@
 package com.boot.dao;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.boot.pojo.User;
 
 public interface UserDao extends JpaRepository<User, Long>{
 
-	public List<User> findByLoginNameAndLoginPassword(String loginName, String loginPassword);
+	public User findByLoginNameAndLoginPassword(String loginName, String loginPassword);
 }
