eb8f5c59dd81953a2a4a288195d80b92b6a482a0
L-PC <1451360190@qq.com>
2018-05-30 15:38:27 +0800

    x

--- src/main/java/com/boot/Application.java
+++ src/main/java/com/boot/Application.java
@@ -2,13 +2,11 @@ package com.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration  
-@EnableAutoConfiguration  
-@ComponentScan  
-//@SpringBootApplication
+@SpringBootApplication
 public class Application {  
 	
       public static void main(String[] args) {  
