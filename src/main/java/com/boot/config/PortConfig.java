a7357c41270cba68fe0fff86c8cfac93a2e6524a
L <1451360190@qq.com>
2019-02-24 13:44:34 +0800

    x

--- src/main/java/com/boot/config/PortConfig.java
+++ src/main/java/com/boot/config/PortConfig.java
@@ -1,13 +1,13 @@
-package com.boot.config;
-
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class PortConfig implements EmbeddedServletContainerCustomizer {
-    @Override
-    public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
-        configurableEmbeddedServletContainer.setPort(8088);
-    }
-}
+//package com.boot.config;
+//
+//import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+//import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class PortConfig implements EmbeddedServletContainerCustomizer {
+//    @Override
+//    public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
+//        configurableEmbeddedServletContainer.setPort(8088);
+//    }
+//}
